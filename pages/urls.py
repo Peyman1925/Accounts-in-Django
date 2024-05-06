552d5f0cb547968065c81390cd09c70da0361ac8
Peyman1925 <72814353+Peyman1925@users.noreply.github.com>
2024-05-06 22:15:04 +0330

    Update urls.py

--- pages/urls.py
+++ pages/urls.py
@@ -2,11 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    
     path('', home, name='home'),
-    # path('add/', add_question, name='add_question'),
-    # path('test', quiz_view, name='quiz_home'),  # مسیر صفحه آزمون
-    # path('submit/', submit_quiz, name='submit_quiz'),  # مسیر برای ارسال فرم آزمون
-    # path('results/', quiz_results, name='quiz_results'),  # مسیر برای نمایش نتایج آزمون
-    
 ]
