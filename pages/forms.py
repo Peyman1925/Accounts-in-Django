7d6dc26e5f0d2b778cb412b8e24974ad9553a6ff
Peyman1925 <72814353+Peyman1925@users.noreply.github.com>
2024-05-06 22:15:30 +0330

    Update forms.py

--- pages/forms.py
+++ pages/forms.py
@@ -1,11 +1,1 @@
 from django import forms
-# from .models import Choice
-
-# class QuestionForm(forms.Form):
-#     choice = forms.ModelChoiceField(queryset=Choice.objects.all(), widget=forms.RadioSelect)
-from .models import *
-
-# class QuestionForm(forms.ModelForm):
-#     class Meta:
-#         model = Question
-#         fields = ['question_text', 'correct_answer']
